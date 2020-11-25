ae21d3744f7c192f0a22f079a09f8a1dbf2a78fe
fesibisyatta <mka37b660cs1@icloud.com>
2020-11-25 20:59:34 +0900

    change params

--- NonlinearProgramming.py
+++ NonlinearProgramming.py
@@ -1,11 +1,11 @@
 # 1変数の場合のバックトラック法を用いた最急降下法
 def f(x):
     return 3*x**2
-
+# 微分
 def df(x):
     return 6*x
 
-def GradientDescent(x):
+def GradientDescentMethod(x):
     for i in range(n):
         delta = - df(x)**2
         if i == 0:
@@ -13,17 +13,14 @@ def GradientDescent(x):
         # Armijoの条件
         while f(x + alpha * delta) > f(x) + c * alpha * delta:
             alpha = rho * alpha
-            print(alpha)
-        x = x  + alpha * delta
+        x = x + alpha * delta
     return f(x)
 
-
-if __name__ = '__main__':
+if __name__ == '__main__':
     x = 10
     n = 10
-    alpha_bar = 0.1
-    c = 0.8
-    rho = 0.3
+    rho = 0.39
+    c = 0.08
+    alpha_bar = 0.01
     gamma = 0.4
-
-    print(GradientDescent(x))
+    print(GradientDescentMethod(x))
